5373ce25bd892a9def3d3615a3981ad233477416
jimmy.jordan@gmail.com <jimmy.jordan@gmail.com>
2025-05-28 20:19:08 +0200

    JImmy JOrdan

--- main.c
+++ main.c
@@ -6,8 +6,43 @@ int main()
     // The Line below must *NOT* be changed or test will fail!
     srand(1);
 
-   
-    // Your code goes here
+    // variables
+    int dice_rolls[100];
+    int sum = 0, ones = 0, twos = 0, threes = 0, fours = 0, fives = 0, sixes = 0;
+
+    // roll dice the dices
+    for (int i = 0; i < 100; i++)
+    {
+        dice_rolls[i] = (rand() % 6) + 1;
+        sum += dice_rolls[i];
+
+        switch (dice_rolls[i])
+        {
+        case 1:
+            ones++;
+            break;
+        case 2:
+            twos++;
+            break;
+        case 3:
+            threes++;
+            break;
+        case 4:
+            fours++;
+            break;
+        case 5:
+            fives++;
+            break;
+        case 6:
+            sixes++;
+            break;
+        }
+    }
+
+    // print
+    printf("%d\n%d\n%d\n%d\n%d\n%d\n", ones, twos, threes, fours, fives, sixes);
+    printf("%d\n", sum);
+    printf("%f\n", (float)sum / 100);
 
     return 0;
 }
